c7e6583556a009199f47d9c5cfa8d71af0b35a53
Adam Bien <abien@adam-bien.com>
2015-02-17 12:08:44 +0100

    Validate the contents of the database

--- javaeetesting/src/test/java/com/airhacks/testing/workshops/entity/WorkshopIT.java
+++ javaeetesting/src/test/java/com/airhacks/testing/workshops/entity/WorkshopIT.java
@@ -3,6 +3,7 @@ package com.airhacks.testing.workshops.entity;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import static org.junit.Assert.assertNotNull;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -25,8 +26,12 @@ public class WorkshopIT {
     @Test
     public void crud() {
         this.tx.begin();
-        this.em.persist(new Workshop("testing"));
+        final String key = "testing";
+        this.em.persist(new Workshop(key));
         this.tx.commit();
+
+        Workshop found = this.em.find(Workshop.class, key);
+        assertNotNull(found);
     }
 
 }
