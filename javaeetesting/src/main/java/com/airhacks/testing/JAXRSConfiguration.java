f6e4a8d49ab94fcfc5ff9153bba4130f13398c28
Adam Bien <abien@adam-bien.com>
2015-02-17 13:06:35 +0100

    Application is deployable

--- javaeetesting/src/main/java/com/airhacks/testing/JAXRSConfiguration.java
+++ javaeetesting/src/main/java/com/airhacks/testing/JAXRSConfiguration.java
@@ -1,12 +1,13 @@
 package com.airhacks.testing;
 
 import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
 
 /**
  *
  * @author airhacks.com
  */
 @ApplicationPath("resources")
-public class JAXRSConfiguration {
+public class JAXRSConfiguration extends Application {
 
 }
