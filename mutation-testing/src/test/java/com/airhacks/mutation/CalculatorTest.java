a8a0de0433586da2ffc08bcd4c1be71a7b10bbd0
Adam Bien <abien@adam-bien.com>
2015-02-17 11:30:33 +0100

    Mutation testing is about quality

--- mutation-testing/src/test/java/com/airhacks/mutation/CalculatorTest.java
+++ mutation-testing/src/test/java/com/airhacks/mutation/CalculatorTest.java
@@ -1,5 +1,8 @@
 package com.airhacks.mutation;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertThat;
 import org.junit.Test;
 
 /**
@@ -15,10 +18,9 @@ public class CalculatorTest {
         Calculator cut = new Calculator(a, b);
         cut.multiply();
         Integer result = cut.getResult();
-        /*
+
         assertNotNull(result);
         assertThat(result, is(a * b));
-         */
     }
 
 }
