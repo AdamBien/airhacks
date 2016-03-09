7e35ea4927eacf83db07660694c2a05bafd725d6
Adam Bien <abien@adam-bien.com>
2016-03-09 14:12:02 +0100

    Dependency Injection sample

--- steaks/src/main/java/com/airhacks/launch/rest/Steaks.java
+++ steaks/src/main/java/com/airhacks/launch/rest/Steaks.java
@@ -1,5 +1,8 @@
 package com.airhacks.launch.rest;
 
+import com.airhacks.launch.services.SteakService;
+import javax.ejb.Stateless;
+import javax.inject.Inject;
 import javax.json.Json;
 import javax.json.JsonObject;
 import javax.ws.rs.GET;
@@ -9,13 +12,17 @@ import javax.ws.rs.Path;
  *
  * @author airhacks.com
  */
+@Stateless
 @Path("steaks")
 public class Steaks {
+
+    @Inject
+    SteakService service;
 
     @GET
     public JsonObject all() {
         return Json.createObjectBuilder().
-                add("type", "medium").
+                add("type", service.steaks()).
                 build();
     }
 
---
+++ steaks/src/main/java/com/airhacks/launch/services/SteakService.java
@@ -0,0 +1,16 @@
+package com.airhacks.launch.services;
+
+import javax.ejb.Stateless;
+
+/**
+ *
+ * @author airhacks.com
+ */
+@Stateless
+public class SteakService {
+
+    public String steaks() {
+        return "bloody";
+    }
+
+}
