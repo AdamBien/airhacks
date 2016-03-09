5ee86200beace1b06ee11b2a35f8d64d22db2abd
Adam Bien <abien@adam-bien.com>
2016-03-09 09:42:27 +0100

    Hello Servlet

---
+++ hello-servlet/src/main/java/com/airhacks/HelloWOrld.java
@@ -0,0 +1,21 @@
+package com.airhacks;
+
+import java.io.IOException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author airhacks.com
+ */
+@WebServlet(name = "HelloWorld", urlPatterns = {"/HelloWorld"})
+public class HelloWOrld extends HttpServlet {
+
+    @Override
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.getWriter().print("hello world " + System.currentTimeMillis());
+    }
+
+}
