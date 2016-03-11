4d2948ea98e56d8d588ee0f1e47e813b866a4d20
Adam Bien <abien@adam-bien.com>
2016-03-11 11:16:48 +0100

    ParallelStream is working

--- java-samples/src/test/java/com/airhacks/ParallelStreamTest.java
+++ java-samples/src/test/java/com/airhacks/ParallelStreamTest.java
@@ -1,8 +1,12 @@
 package com.airhacks;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import org.junit.Before;
+import org.junit.Test;
 
 /**
  *
@@ -15,6 +19,16 @@ public class ParallelStreamTest {
     @Before
     public void init() {
         this.client = ClientBuilder.newClient();
+    }
+
+    @Test
+    public void crawler() {
+        List<String> uris = Arrays.asList("http://www.google.de", "http://www.pc-agrar.de");
+        uris.forEach(System.out::println);
+        List<String> contents = uris.parallelStream().
+                map(this::fetchContent).
+                collect(Collectors.toList());
+        contents.forEach(System.out::println);
     }
 
     public String fetchContent(String uri) {
