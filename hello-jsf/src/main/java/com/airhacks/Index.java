613ee39b2dc2b148ea2ac9e9479af6744eb9a7bc
Adam Bien <abien@adam-bien.com>
2016-03-11 11:41:30 +0100

    JSF explained

--- hello-jsf/src/main/java/com/airhacks/Index.java
+++ hello-jsf/src/main/java/com/airhacks/Index.java
@@ -1,5 +1,6 @@
 package com.airhacks;
 
+import javax.annotation.PostConstruct;
 import javax.enterprise.inject.Model;
 import javax.inject.Inject;
 import javax.validation.constraints.Size;
@@ -14,8 +15,15 @@ public class Index {
     @Size(min = 3, max = 5)
     private String name;
 
+    private int age;
+
     @Inject
     BetriebService service;
+
+    @PostConstruct
+    public void init() {
+        this.age = 42;
+    }
 
     public String getName() {
         return name;
@@ -23,6 +31,14 @@ public class Index {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
     }
 
     public Object save() {
