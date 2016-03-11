2f87cc6cd340ff4f5cc28902a4061fd740d77bea
Adam Bien <abien@adam-bien.com>
2016-03-11 09:58:59 +0100

    Lambdas explained

--- java-samples/src/test/java/com/airhacks/LambdaTest.java
+++ java-samples/src/test/java/com/airhacks/LambdaTest.java
@@ -1,5 +1,8 @@
 package com.airhacks;
 
+import java.util.function.Function;
+import java.util.function.Predicate;
+import static org.junit.Assert.assertTrue;
 import org.junit.Test;
 
 /**
@@ -10,7 +13,31 @@ public class LambdaTest {
 
     @Test
     public void methodReferences() {
+        Runnable r = this::beliebig;
+        r.run();
+    }
 
+    @Test
+    public void lambda() {
+        Runnable r = () -> System.out.println("hey joe");
+        r.run();
+    }
+
+    @Test
+    public void functions() {
+        Function<String, String> function = (t) -> t + "duke";
+        String result = function.apply("chief");
+        System.out.println("result = " + result);
+    }
+
+    @Test
+    public void filter() {
+        Predicate<String> filter = (a) -> a.equalsIgnoreCase("duke");
+        assertTrue(filter.test("duke"));
+    }
+
+    public void beliebig() {
+        System.out.println("hey joe");
     }
 
 }
