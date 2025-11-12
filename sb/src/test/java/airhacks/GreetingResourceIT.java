680f89b3a05d096cd9532b8ca88b7dc576e052d0
Adam Bien <abien@adam-bien.com>
2025-11-12 10:44:34 +0100

    link added

--- sb/src/test/java/airhacks/GreetingResourceIT.java
+++
@@ -1,8 +0,0 @@
-package airhacks;
-
-import io.quarkus.test.junit.QuarkusIntegrationTest;
-
-@QuarkusIntegrationTest
-class GreetingResourceIT extends GreetingResourceTest {
-    // Execute the same tests but in packaged mode.
-}
