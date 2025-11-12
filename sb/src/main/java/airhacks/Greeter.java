344de7a7f8ee0df1dce278bb85eb76bb6d95346b
Adam Bien <abien@adam-bien.com>
2025-11-12 11:03:23 +0100

    scoped explained

--- sb/src/main/java/airhacks/Greeter.java
+++ sb/src/main/java/airhacks/Greeter.java
@@ -2,15 +2,28 @@ package airhacks;
 
 import org.eclipse.microprofile.config.inject.ConfigProperty;
 
-import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+import jakarta.enterprise.context.RequestScoped;
 import jakarta.inject.Inject;
 
-@ApplicationScoped
+@RequestScoped
 public class Greeter {
 
     @Inject
     @ConfigProperty(name="first.name",defaultValue = "james")
     String firstName;
+
+    @PostConstruct
+    public void init(){
+        System.out.println(this.getClass() + " init");
+    }
+
+    @PreDestroy
+    public void destroy(){
+        System.out.println(this.getClass() + " destroy");
+    }
+
     
     public String greet(){
         System.out.println("hello, duke");
--- sb/src/main/java/airhacks/GreetingResource.java
+++ sb/src/main/java/airhacks/GreetingResource.java
@@ -1,20 +1,36 @@
 package airhacks;
 
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 
+@ApplicationScoped
 @Path("/hello")
 public class GreetingResource {
 
     @Inject
-    Greeter greeter;
+    Greeter tweeter;
+
+    @PostConstruct
+    public void init(){
+        System.out.println(this.getClass() + " init");
+    }
+
+    @PreDestroy
+    public void destroy(){
+        System.out.println(this.getClass() + " destroy");
+    }
+
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
-        return this.greeter.greet();
+        System.out.println("- " + this.tweeter.getClass());
+        return this.tweeter.greet();
     }
 }
