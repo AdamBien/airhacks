d329200229e19ecc55175b6bd155163f2f0c4da3
Adam Bien <abien@adam-bien.com>
2025-11-12 11:25:26 +0100

    basic REST explained

--- sb/src/main/java/airhacks/GreetingResource.java
+++ sb/src/main/java/airhacks/GreetingResource.java
@@ -5,12 +5,16 @@ import jakarta.annotation.PreDestroy;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
 
 @ApplicationScoped
-@Path("/hello")
+@Path("greetings")
+@Produces(MediaType.TEXT_PLAIN)
 public class GreetingResource {
 
     @Inject
@@ -28,9 +32,21 @@ public class GreetingResource {
 
 
     @GET
-    @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
         System.out.println("- " + this.tweeter.getClass());
         return this.tweeter.greet();
+    }
+
+    @GET
+    @Path("{id}-{name}")
+    public String byId(@PathParam("id") String id,@PathParam("name") String name) {
+        System.out.println("- " +  id + " " + name);
+        return this.tweeter.greet() + " - " +  id + " " + name;
+    }
+
+    @POST
+    public Response hello(String message) {
+        System.out.println("- " + message);
+        return Response.noContent().build();
     }
 }
